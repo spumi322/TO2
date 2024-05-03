df335c447283dbc65d4a06bae9696c4b2a84c282
spumi322 <martonfeher@protonmail.com>
2024-05-03 12:23:46 +0200

    add initial migration

--- src/webAPI/Program.cs
+++ src/webAPI/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 namespace TO2
 {
     public class Program
@@ -5,7 +8,23 @@ namespace TO2
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<TO2DbContext>(options =>
+            {
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TOApp v2"));
+            }
 
             app.MapGet("/", () => "Hello World!");
 
